69721bc52b954fe053dc1fe0065a108b9a081970
LinhLinhCute <nttlinh.tvb2019@gmail.com>
2023-08-30 15:49:22 +0700

    add Bai 4

--- 30_NgoThiThuyLinh_Buoi3_OOP/Program.cs
+++ 30_NgoThiThuyLinh_Buoi3_OOP/Program.cs
@@ -10,19 +10,31 @@ namespace _30_NgoThiThuyLinh_Buoi3_OOP
     {
         static void Main(string[] args)
         {
-            TamGiac TamGiac1 = new TamGiac(3, 4, 5);
-            TamGiac TamGiac2 = new TamGiac(8, 8, 5);
-            TamGiac TamGiac3 = new TamGiac(4, 4, 4);
-            TamGiac TamGiac4 = new TamGiac(5,3 ,6 );
-            TamGiac TamGiac5 = new TamGiac(5, 5, 5);
-            Console.WriteLine("Thong so Tam Giac");
-            Console.WriteLine("ma\tmb\tmc\tLoai Tam Giac\tChu Vi\tDien Tich");
-            Console.WriteLine("==============================================");
-            Console.WriteLine(TamGiac1.Xuat());
-            Console.WriteLine(TamGiac2.Xuat());
-            Console.WriteLine(TamGiac3.Xuat());
-            Console.WriteLine(TamGiac4.Xuat());
-            Console.WriteLine(TamGiac5.Xuat());
+            //Bai 3
+            //TamGiac TamGiac1 = new TamGiac(3, 4, 5);
+            //TamGiac TamGiac2 = new TamGiac(8, 8, 5);
+            //TamGiac TamGiac3 = new TamGiac(4, 4, 4);
+            //TamGiac TamGiac4 = new TamGiac(5,3 ,6 );
+            //TamGiac TamGiac5 = new TamGiac(5, 5, 5);
+            //Console.WriteLine("Thong so Tam Giac");
+            //Console.WriteLine("ma\tmb\tmc\tLoai Tam Giac\tChu Vi\tDien Tich");
+            //Console.WriteLine("==============================================");
+            //Console.WriteLine(TamGiac1.Xuat());
+            //Console.WriteLine(TamGiac2.Xuat());
+            //Console.WriteLine(TamGiac3.Xuat());
+            //Console.WriteLine(TamGiac4.Xuat());
+            //Console.WriteLine(TamGiac5.Xuat());
+            //Bai 4
+            Vehicle xe1 = new Vehicle("NgoThiThuyLinh", "Roll-Royce", 10651252500, 6000);
+            Vehicle xe2 = new Vehicle("Vu Minh Chien", "Lamborghini", 26000000000, 6498);
+            Vehicle xe3 = new Vehicle("Ngo Ai Linh", "Ferrari  ", 20000000000, 5300);
+            Vehicle xe4 = new Vehicle("VuThiMinhTam", "Mercedes", 10000000000, 5300);
+            Console.WriteLine("Chu Xe\tLoai Xe\t\tTri Gia\t\t\tDung Tich\tThue Tri Gia");
+            Console.WriteLine("========================================================");
+            Console.WriteLine(xe1.Xuat());
+            Console.WriteLine(xe2.Xuat());
+            Console.WriteLine(xe3.Xuat());
+            Console.WriteLine(xe4.Xuat());
             Console.ReadLine();
         }
     }
---
+++ 30_NgoThiThuyLinh_Buoi3_OOP/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _30_NgoThiThuyLinh_Buoi3_OOP
+{
+    class Vehicle
+    {
+        private string chuxe;
+        private string loaixe;
+        private double trigia;
+        private int dungtich;
+
+        public string ChuXe
+        {
+            get { return chuxe; }
+            set{chuxe=value;}
+        }
+        public string LoaiXe
+        {
+            get { return loaixe; }
+            set { loaixe = value; }
+        }
+        public double TriGia
+        {
+            get { return trigia; }
+            set
+            {
+                if (value >= 0)
+                    trigia = value;
+            }
+        }
+        public int DungTich
+        {
+            get { return dungtich; }
+            set { 
+                if(value>=0)
+                 dungtich = value; 
+            }
+        }
+        public Vehicle()
+        {
+            chuxe = "";
+            loaixe = "";
+            trigia = 0;
+            dungtich = 0;
+        }
+        public Vehicle(string chuxe, string loaixe, double trigia, int dungtich)
+        {
+            this.chuxe = chuxe;
+            this.loaixe = loaixe;
+            this.trigia = trigia;
+            this.dungtich = dungtich;
+        }
+        public double TinhThue()
+        {
+            if (trigia < 100)
+            {
+                return trigia * 1 / 100;
+            }
+            else if (trigia >= 200)
+                return trigia * 3 / 100;
+            return trigia * 5 / 100;
+        }
+        public string Xuat()
+        {
+            return chuxe + '\t' + loaixe + '\t' + dungtich.ToString() + "\t\t" + trigia.ToString() + '\t' + TinhThue();
+        }
+    }
+}
