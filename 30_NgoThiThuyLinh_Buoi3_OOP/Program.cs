14dd8386d2d21fe105cba8bbb98350d0d2d11039
LinhLinhCute <nttlinh.tvb2019@gmail.com>
2023-08-30 15:56:59 +0700

    add lan2 bai4

--- 30_NgoThiThuyLinh_Buoi3_OOP/Program.cs
+++ 30_NgoThiThuyLinh_Buoi3_OOP/Program.cs
@@ -10,7 +10,7 @@ namespace _30_NgoThiThuyLinh_Buoi3_OOP
     {
         static void Main(string[] args)
         {
-            //Bai 3
+                 //Bai 3
             //TamGiac TamGiac1 = new TamGiac(3, 4, 5);
             //TamGiac TamGiac2 = new TamGiac(8, 8, 5);
             //TamGiac TamGiac3 = new TamGiac(4, 4, 4);
@@ -24,12 +24,12 @@ namespace _30_NgoThiThuyLinh_Buoi3_OOP
             //Console.WriteLine(TamGiac3.Xuat());
             //Console.WriteLine(TamGiac4.Xuat());
             //Console.WriteLine(TamGiac5.Xuat());
-            //Bai 4
+                //Bai 4
             Vehicle xe1 = new Vehicle("NgoThiThuyLinh", "Roll-Royce", 10651252500, 6000);
             Vehicle xe2 = new Vehicle("Vu Minh Chien", "Lamborghini", 26000000000, 6498);
             Vehicle xe3 = new Vehicle("Ngo Ai Linh", "Ferrari  ", 20000000000, 5300);
             Vehicle xe4 = new Vehicle("VuThiMinhTam", "Mercedes", 10000000000, 5300);
-            Console.WriteLine("Chu Xe\tLoai Xe\t\tTri Gia\t\t\tDung Tich\tThue Tri Gia");
+            Console.WriteLine("Chu Xe\t\tLoai Xe\t\tTri Gia\t\tDung Tich\tThue Tri Gia");
             Console.WriteLine("========================================================");
             Console.WriteLine(xe1.Xuat());
             Console.WriteLine(xe2.Xuat());
